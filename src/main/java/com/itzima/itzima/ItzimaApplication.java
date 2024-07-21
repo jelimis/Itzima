709fb3fb28ed7d38b5cd2b8841f18476d3294d22
Yunjoung0730 <151517628+Yunjoung0730@users.noreply.github.com>
2024-07-21 17:18:12 +0900

    Chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/itzima/itzima/ItzimaApplication.java
@@ -0,0 +1,13 @@
+package com.itzima.itzima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItzimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItzimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itzima/itzima/ItzimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzima.itzima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItzimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
